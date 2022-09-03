a28bbf3da41c7d68eeb4b6c7146697542747da85
Ruby Truong <rubytruong1011@gmail.com>
2022-09-03 14:08:42 -0400

    Form

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove("hidden");
+} 
 
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove("hidden");
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
